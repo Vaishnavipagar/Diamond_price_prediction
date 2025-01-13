6d83bb385b755f0f912ef836a73ae619ab22fbd4
Vaishnavipagar <vaishanvipagar7@gmail.com>
2025-01-13 14:16:19 +0530

    file

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from flask import Flask,request,render_template,jsonify
 
 app=Flask(__name__)
 
-
+'''
 @app.route('/')
 def home_page():
-    return render_template("index.html")
+    return render_template("index.html")'''
 
 
 @app.route("/predict",methods=["GET","POST"])
