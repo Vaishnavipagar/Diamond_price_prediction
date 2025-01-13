84eb9c06e41d55709208289e2611537f8995c523
Vaishnavipagar <vaishanvipagar7@gmail.com>
2025-01-13 14:10:39 +0530

    update the file

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ from flask import Flask,request,render_template,jsonify
 app=Flask(__name__)
 
 
-@app.route("/",methods=["GET","POST"])
-def predict_data():
+@app.route('/')
+def home_page():
+    return render_template("index.html")
+
+
+@app.route("/predict",methods=["GET","POST"])
+def predict_datapoint():
     if request.method == "GET":
         return render_template("form.html")
     
@@ -23,7 +28,7 @@ def predict_data():
             color= request.form.get('color'),
             clarity = request.form.get('clarity')
         )
-        
+        # this is my final data
         final_data=data.get_data_as_dataframe()
         
         predict_pipeline=predictPipeline()
@@ -34,5 +39,6 @@ def predict_data():
         
         return render_template("result.html",final_result=result)
 
-if __name__ == '_main_':
-    app.run(debug=True)
+#execution begin
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=80)
